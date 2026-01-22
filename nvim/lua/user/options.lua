9c3e59f3afeab260af28a5461162a5c66da106ee
0xdeadf1sh <0xdeadf1sh@tuta.io>
2026-01-22 11:57:25 +0400

    Update neovim config

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,31 +1,53 @@
-local opt = vim.opt
-
 -- General
-opt.encoding = "utf-8"
-opt.hidden = true
-opt.ttyfast = true
-opt.updatetime = 300
-opt.signcolumn = "yes"
-opt.clipboard = "unnamedplus"
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = false
+
+-- Options
+vim.o.encoding = "utf-8"
+vim.o.hidden = true
+vim.o.ttyfast = true
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
+vim.o.signcolumn = "yes"
+vim.o.undofile = true
+vim.o.splitright = true
+vim.o.splitbelow = true
+vim.o.list = true
+vim.o.inccommand = 'split'
+vim.o.scrolloff = 10
+vim.o.confirm = true
+vim.o.showmode = false
+vim.o.breakindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- Numbers
-opt.number = true
-opt.relativenumber = true
+vim.o.number = true
+vim.o.relativenumber = true
 
 -- Tabs and Indentation
-opt.expandtab = true
-opt.tabstop = 4
-opt.softtabstop = 4
-opt.shiftwidth = 4
-opt.cindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.cindent = true
 
 -- Search
-opt.hlsearch = true
-opt.incsearch = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
 
 -- Appearance
-opt.termguicolors = true
-opt.cursorline = true
+vim.o.termguicolors = true
+vim.o.cursorline = true
+
+-- Mouse
+vim.o.mouse = 'a'
+
+-- Sync with OS clipboard
+vim.schedule(function()
+  vim.o.clipboard = 'unnamedplus'
+end)
 
 -- Comments
 vim.cmd('iabbrev ee ///////////////////////////////////////////////////////////////////////////')
